8250b5d0b4eb75f697fb6ef1b2d682972dfdb4d7
cbdyzj <cbdyzj@jianzhao.org>
2021-10-27 20:05:50 +0800

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,16 +1,5 @@
-const htm = window.htm
-const { h, render, Component, } = window.preact
-const { useState, useRef, useEffect,useCallback } = window.preactHooks
+export const { h, render, Component, } = window['preact']
+export const { useState, useRef, useEffect, useCallback } = window['preactHooks']
 
-const html = htm.bind(h)
-
-export {
-    h,
-    html,
-    render,
-    useState,
-    useEffect,
-    useRef,
-    useCallback,
-    Component,
-}
+const htm = window['htm']
+export const html = htm.bind(h)
